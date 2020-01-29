6cc63167c9d7a231ff4a4ec3bbe29159259a92b5
LAWVANYA45 <60418766+LAWVANYA45@users.noreply.github.com>
2020-01-29 10:56:51 +0530

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,16 @@
+def add(a,b):
+    sum=a+b
+    print(sum)
+def sub(a,b):
+    diff=a-b
+    print(diff)
+def mul(a,b):
+    multiply=a*b
+    print(multiply)
+def div(a,b):
+    divide=a/b
+    print(divide)
+add(20,5)
+sub(10,4)
+mul(5,6)
+div(40,8)
